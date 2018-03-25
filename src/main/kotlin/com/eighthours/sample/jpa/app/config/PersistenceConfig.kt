2d007d26963f465535d35f71538a9f016de20f0a
motch0214 <motch0214@gmail.com>
2018-03-26 03:24:45 +0900

    fix: enable transaction and use HikariCP

--- src/main/kotlin/com/eighthours/sample/jpa/app/config/PersistenceConfig.kt
+++ src/main/kotlin/com/eighthours/sample/jpa/app/config/PersistenceConfig.kt
@@ -1,27 +1,26 @@
 package com.eighthours.sample.jpa.app.config
 
-import org.springframework.beans.factory.FactoryBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.orm.jpa.JpaTransactionManager
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
 import org.springframework.transaction.PlatformTransactionManager
+import org.springframework.transaction.annotation.EnableTransactionManagement
 import javax.persistence.EntityManager
 import javax.persistence.EntityManagerFactory
+import javax.persistence.Persistence
 
 
 @Configuration
 @EnableJpaRepositories("com.eighthours.sample.jpa.app.repository")
-class JpaConfig {
+@EnableTransactionManagement
+class PersistenceConfig {
 
     val persistenceUnitName = "sample"
 
     @Bean
-    fun entityManagerFactory(): FactoryBean<EntityManagerFactory> {
-        return LocalContainerEntityManagerFactoryBean().apply {
-            persistenceUnitName = this@JpaConfig.persistenceUnitName
-        }
+    fun entityManagerFactory(): EntityManagerFactory {
+        return Persistence.createEntityManagerFactory(persistenceUnitName)
     }
 
     @Bean
--- src/test/kotlin/com/eighthours/sample/jpa/app/TestApplication.kt
+++ src/test/kotlin/com/eighthours/sample/jpa/app/TestApplication.kt
@@ -1,17 +1,17 @@
 package com.eighthours.sample.jpa.app
 
 import com.eighthours.sample.jpa.app.config.DomainConfig
-import com.eighthours.sample.jpa.app.config.JpaConfig
+import com.eighthours.sample.jpa.app.config.PersistenceConfig
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 
 
 @Configuration
-@Import(TestJpaConfig::class, DomainConfig::class)
+@Import(TestPersistenceConfig::class, DomainConfig::class)
 class TestApplication
 
 
 @Configuration
-class TestJpaConfig : JpaConfig() {
+class TestPersistenceConfig : PersistenceConfig() {
     override val persistenceUnitName: String get() = "sample-test"
 }
