c2644bbb277d6fadb06badb710c0a2d757fd6fe8
motch0214 <motch0214@gmail.com>
2018-03-26 05:31:58 +0900

    test: named query

--- src/test/kotlin/com/eighthours/sample/jpa/app/TestApplication.kt
+++ src/test/kotlin/com/eighthours/sample/jpa/app/TestApplication.kt
@@ -2,8 +2,11 @@ package com.eighthours.sample.jpa.app
 
 import com.eighthours.sample.jpa.app.config.DomainConfig
 import com.eighthours.sample.jpa.app.config.PersistenceConfig
+import org.springframework.beans.factory.BeanFactory
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
+import kotlin.reflect.KClass
 
 
 @Configuration
@@ -15,3 +18,8 @@ class TestApplication
 class TestPersistenceConfig : PersistenceConfig() {
     override val persistenceUnitName: String get() = "sample-test"
 }
+
+
+fun initContext() = AnnotationConfigApplicationContext(TestApplication::class.java)
+
+operator fun <T : Any> BeanFactory.get(clazz: KClass<T>): T = this.getBean(clazz.java)
--- src/test/kotlin/com/eighthours/sample/jpa/domain/TaskUsecaseTest.kt
+++ src/test/kotlin/com/eighthours/sample/jpa/domain/TaskUsecaseTest.kt
@@ -1,12 +1,12 @@
 package com.eighthours.sample.jpa.domain
 
-import com.eighthours.sample.jpa.app.TestApplication
+import com.eighthours.sample.jpa.app.get
+import com.eighthours.sample.jpa.app.initContext
 import com.eighthours.sample.jpa.domain.usecase.TaskUsecase
 import com.eighthours.sample.jpa.domain.usecase.UserUsecase
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.BeforeClass
 import org.junit.Test
-import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 
 class TaskUsecaseTest {
@@ -20,10 +20,10 @@ class TaskUsecaseTest {
         @BeforeClass
         @JvmStatic
         fun setup() {
-            val context = AnnotationConfigApplicationContext(TestApplication::class.java)
+            val context = initContext()
 
-            userUsecase = context.getBean(UserUsecase::class.java)
-            taskUsecase = context.getBean(TaskUsecase::class.java)
+            userUsecase = context[UserUsecase::class]
+            taskUsecase = context[TaskUsecase::class]
         }
     }
 
@@ -33,7 +33,10 @@ class TaskUsecaseTest {
 
         val task = taskUsecase.createTask("Task A", user.name)
 
-        print(task.id)
+        println(task)
         assertThat(task.name).isEqualTo("Task A")
+
+        val tasks = taskUsecase.listUpTask()
+        println(tasks)
     }
 }
